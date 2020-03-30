41a983bd07f347edcd23e1c6264f66cb5a201619
John Milligan <jonomilligan@gmail.com>
2020-03-30 19:28:57 +0200

    Our first commit!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func buttonTap(_ sender: Any) {
+        buttonCount += 1
+        print(buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "Succes!"
+        }
+        if buttonCount >= 13 {
+            view.backgroundColor = UIColor.green
+            myLabel.text = "Success again!"
+        }
+        .
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
     }
 
 
